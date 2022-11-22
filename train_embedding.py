6c322bca9d3a524bd7643612d600c3377d7e9aa5
Nathan Heidacker <nathanheidacker@gmail.com>
2022-11-22 13:46:27 +0900

    nathan

--- train_embedding.py
+++ train_embedding.py
@@ -1,26 +1,60 @@
-'''
+# -*- encoding: utf-8 -*-
+
+"""
 Data processing and normalization for OTTO
-'''
+"""
 
+# Standard Imports
 import random
+import pickle
+from enum import Enum
 
+# Third Party Imports
 import numpy as np
 import pandas as pd
-import pickle
-
-from matplotlib import dates
-from IPython.display import display
 from tqdm import tqdm
-from collections import OrderedDict
 import torch
-import matplotlib.pyplot as plt
-import seaborn as sns
+
+
+class Actions(Enum):
+    CLICKS = "clicks"
+    CARTS = "carts"
+    ORDERS = "orders"
+
+
+class ActionIndexer:
+    def __init__(self):
+        self.indexes = {
+            Actions.CLICKS: {
+                Actions.CLICKS: 0,
+                Actions.CARTS: 1,
+                Actions.ORDERS: 2,
+            },
+            Actions.CARTS: {
+                Actions.CLICKS: 3,
+                Actions.CARTS: 4,
+                Actions.ORDERS: 5,
+            },
+            Actions.ORDERS: {
+                Actions.CLICKS: 6,
+                Actions.CARTS: 7,
+                Actions.ORDERS: 8,
+            },
+        }
+
+    def __getitem__(self, actions):
+        action1, action2 = actions
+        return self.indexes[action1][action2]
+
 
 class Embedding:
     def __init__(self) -> None:
-        with open('aids.p', 'rb') as f:
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        with open("aids.p", "rb") as f:
             self.aids = pickle.load(f)
+        self.n_aids = len(self.aids)
+        self.embedding = torch.zeros((self.n_aids, self.n_aids, 9))
 
 
-if __name__ == '__main__':
-    pass
+if __name__ == "__main__":
+    print(Embedding().embedding.shape())
