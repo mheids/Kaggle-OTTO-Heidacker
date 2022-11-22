0f413611ae381cef8af80d9c45d457aea4b05a5a
Nathan Heidacker <nathanheidacker@gmail.com>
2022-11-22 19:00:33 +0900

    cleanup

--- get_aids.py
+++ get_aids.py
@@ -1,33 +1,31 @@
-'''
-Data processing and normalization for OTTO
-'''
+# -*- encoding: utf-8 -*-
 
+"""
+Data processing and normalization for OTTO
+"""
+
+# Standard Imports
 import random
 
+# Third Party Imports
 import numpy as np
 import pandas as pd
 import pickle
-
-from matplotlib import dates
-from IPython.display import display
 from tqdm import tqdm
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 import warnings
-warnings.filterwarnings('ignore')
+
+warnings.filterwarnings("ignore")
 
 if __name__ == "__main__":
-    chunks = pd.read_json('./Data/train.jsonl', lines=True, chunksize=100_000)
+    chunks = pd.read_json("./Data/train.jsonl", lines=True, chunksize=100_000)
 
     aids = set()
 
     for i, chunk in tqdm(enumerate(chunks)):
-        for session, events in zip(chunk['session'].tolist(), \
-            chunk['events'].tolist()):
-
+        for events in chunk["events"]:
             for event in events:
-                aids.add(event['aid'])
+                aids.add(event["aid"])
 
-    with open('aids.p', 'wb+') as f:
+    with open("aids.p", "wb+") as f:
         pickle.dump(aids, f)
